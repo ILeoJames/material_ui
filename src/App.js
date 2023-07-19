32026fe04e02fbf1f41d425da0de15ed8586cf06
Leo James <profile_vladimir@mail.ru>
2023-07-19 20:33:01 +0600

    Create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppBar, Container, Toolbar} from '@material-ui/core';
+import Fab from '@material-ui/core/Fab';
+import EditIcon from '@material-ui/icons/Edit';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="fixed">
+        <Container fixed>
+          <Toolbar>
+          <Fab color="secondary" aria-label="edit">
+        <EditIcon />
+      </Fab>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </div>
   );
 }
